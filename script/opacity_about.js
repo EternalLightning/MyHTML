c86554f06e46f233be2db1ecc735dea4a8856ca3
EternalLightning <EternalLightning_@outlook.com>
2024-04-18 21:55:12 +0800

    Add some information

--- script/opacity_about.js
+++ script/opacity_about.js
@@ -3,7 +3,7 @@ window.addEventListener('scroll', function() {
     var scrollDistance = window.scrollY || document.documentElement.scrollTop;
 
     // 计算透明度
-    var opacity = 0.85 + scrollDistance / 130; // 500是一个透明度变化的阈值，根据需要调整
+    var opacity = 0.85 + scrollDistance / 1000; // 500是一个透明度变化的阈值，根据需要调整
 
     // 更新透明度
     navbar.style.opacity = opacity.toString();
